84d2be2b1140da3d2e370443e4445a0904a578f9
mohamedchadrak <chadrakassani@gmail.com>
2025-12-05 06:50:53 +0100

    V1

---
+++ next.conf.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+    /* config options here */
+    output: "export",
+    assetPrefix: '/nexus/',
+};
+
+export default nextConfig;
